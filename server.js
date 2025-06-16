df19680c2869a6420324966a0c26319ae5b872f4
nabiya <sheikh.nabiyaa@gmail.com>
2025-06-16 17:30:37 +0100

    commit

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const cron = require('node-cron');
 const { sequelize } = require('./models');
 const routes = require('./routes');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const logger = require('./utils/logger');
+const { Seat } = require('./models');
+const { SEAT_STATUS } = require('./utils/constants');
 
 // Loading environment variables
 dotenv.config();
@@ -22,6 +25,13 @@ app.use('/api', routes);
 
 // Applying error handling middleware
 app.use(errorMiddleware);
+
+cron.schedule('*/1 * * * *', async () => {
+  await Seat.update(
+    { status: SEAT_STATUS.AVAILABLE, blockedUntil: null },
+    { where: { status: SEAT_STATUS.BLOCKED, blockedUntil: { [Op.lt]: new Date() } } }
+  );
+});
 
 // Setting up server port
 const PORT = process.env.PORT || 5000;
